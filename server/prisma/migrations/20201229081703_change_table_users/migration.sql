312e94def4f769dec721c25021d63459627e6ca0
ren machida <r-machida@c3reve.co.jp>
2020-12-29 18:55:46 +0900

    #4 ユーザー新規登録実装

---
+++ server/prisma/migrations/20201229081703_change_table_users/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `deleted` on the `users` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `users` DROP COLUMN `deleted`,
+    ADD COLUMN     `deletedAt` DATETIME(3);
